ae665f7fe73171cca11dfafe6f74d58894fbe5de
mateovranych <mateovranych@gmail.com>
2025-10-13 00:46:32 -0300

    cambio en authService, y end en env. /api

--- src/Service/auth-service.ts
+++ src/Service/auth-service.ts
@@ -2,20 +2,25 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { tap } from 'rxjs/operators';
+import { environment } from '../environments/environment';
 
 
-const API_URL = 'https://localhost:7268/api/Auth'; 
+
+
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
+  private apiUrl = `${environment.apiUrl}/Auth`;
+
   constructor(
     private http: HttpClient  ) { }
 
 login(credentials: any): Observable<{ token: string }> { 
-    return this.http.post<{ token: string }>(`${API_URL}/login`, credentials).pipe( 
+    return this.http.post<{ token: string }>(`${this.apiUrl}/login`, credentials).pipe( 
       tap((response) => {        
         localStorage.setItem('jwt_token', response.token); 
       })
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://lanuevabodegaapi-eaexfudzerhwgpg5.brazilsouth-01.azurewebsites.net/index.html'
+  apiUrl: 'https://lanuevabodegaapi-eaexfudzerhwgpg5.brazilsouth-01.azurewebsites.net/api'
 };
