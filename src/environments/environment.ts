dfb6e6003961b925453ecc3ec47735e886c3b89e
mateovranych <mateovranych@gmail.com>
2025-12-07 23:02:46 -0300

    test

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   // apiUrl: 'https://localhost:7268/api'
-  apiUrl: 'https://lanuevabodegaapi-eaexfudzerhwgpg5.brazilsouth-01.azurewebsites.net' 
+  apiUrl: 'https://lanuevabodegaapi-eaexfudzerhwgpg5.brazilsouth-01.azurewebsites.net/api' 
 };
